7ba5a809a8613e5915fea9d1ea4729a7973ff5f1
mse2321 <mse233@gmail.com>
2015-12-07 20:14:58 -0500

    finished up mobile nav and some other updates?

--- site/files/main.js
+++ site/files/main.js
@@ -1,23 +1,8 @@
 $( document ).ready(function() {
 	"use strict";
 
-	$(".fa-bars").hide(hideMobileNav);
-
-	$(".fa-bars").click(showMobileNav);
-
-	$("#button").velocity({
-	    /* Animate a color property to a hex value of red... */
-	    color: "#ff0000";
-	});
-
-	/* Shows Navigation when in a mobile view */
-	function hideMobileNav () {
-		$("#nav-options").hide();
-	}
-
-	/* Shows Navigation when in a mobile view */
-	function showMobileNav () {
-		$("#nav-options").show();
-	}
+	$( ".fa-bars" ).click(function() {
+    	$( "#nav-options" ).slideToggle( "slow" );
+  	});
 
 });
